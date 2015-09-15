4f6498fbc0e63e814a453ede99eada5de93c8f03
sani_shrestha <shresstha.s@gmail.com>
2015-09-15 20:19:12 +0545

    first insert

---
+++ GradleFirstAssignment/src/main/java/com/lftechnology/logviewer/HelloWorld.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.lftechnology.logviewer;
+
+/**
+ *
+ * @author sani
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello Team memebers you can edit here !!!!!!!!");
+    }
+}
